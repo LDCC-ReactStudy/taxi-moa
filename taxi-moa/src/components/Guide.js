844eeb23d180da706dc5a6667ec9ebc530849435
Mingyu-Han <bmind2001@gmail.com>
2022-11-07 19:38:06 +0900

    메인화면에 진행상태 컴포넌트 추가

---
+++ taxi-moa/src/components/Guide.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Guide() {
+  return <div>가이드구간</div>
+}
+
+export default Guide;
--- taxi-moa/src/components/Main.js
+++ taxi-moa/src/components/Main.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import TaxiCard from './TaxiCard';
+import Status from './Status';
+import Guide from './Guide'
 import styles from './Main.module.css';
 
 function Main() {
-  return <div className={styles.Background}>
+  return <div>
+    <div className={styles.GuideStatusDiv}>
+      <tr>
+        <td><Guide/></td>
+        <td><Status/></td>
+      </tr>
+    </div>
+    <div className={styles.Background}> 
     <TaxiCard/>
+    </div>
   </div>
 }
 
